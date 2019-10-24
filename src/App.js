082836070205260408780fd3f9b7ce0069f44214
Genevieve Crow <genevieve.crow@submittable.com>
2019-10-23 18:32:24 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import avatar from './gcrowavatar.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={avatar} alt="user" />
         <p>
-          Hi. This is where the magic happens.
+          Hi. This is where the magic happens. Testing if this changes anything.
         </p>
       </header>
     </div>
