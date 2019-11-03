fb81673fda03d42d438e209ac9487cb5f25acefe
Genevieve Crow <genevieve.crow@submittable.com>
2019-11-03 09:50:10 -0700

    Add date picker component to user dashboard

--- src/AddClimb.js
+++ src/AddClimb.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react';
 import './AddClimb.css'
+import DatePicker from 'react-date-picker'
 
-function Chart() {
-  return (
-    <div className="addClimbContainer">
-      <p className="addClimbTitle">Add climb</p>
-      <div className="inputOptions">
-        Date <input className="routeDate option" id="routeDate"></input>
-        Route name <input className="routeName option" id="routeName"></input>
-        Grade <input className="routeGrade option" id="routeGrade"></input>
-        Notes <input className="routeNotes option" id="routeNotes"></input>
+class Chart extends Component {
+  state = {
+    date: new Date(),
+  }
+
+  onChange = date => this.setState({ date })
+
+  render() {
+    return (
+      <div className="addClimbContainer">
+        <p className="addClimbTitle">Add climb</p>
+        <div className="inputOptions">
+          Date <DatePicker 
+            className="routeDate option" 
+            id="routeDate" 
+            onChange={this.onChange}
+            value={this.state.date}
+            />
+          <span id="numClimbs">Climbs <input className="routeGrade option" id="routeGrade"></input></span>
+          Notes <input className="routeNotes option" id="routeNotes"></input>
+        </div>
+        <button className="addClimb">Add session</button>
       </div>
-      <button className="addClimb">Add climb</button>
-    </div>
-  );
+    );
+  }
 }
 
 export default Chart
